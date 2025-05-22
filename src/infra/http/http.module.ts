1ebdf0dd0e88f5f6de072d92dbea6e0fa546d8c2
danrlleimiranda <danmiranda14@gmail.com>
2025-05-22 16:38:17 -0300

    feat: somethings

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { CompanyUseCaseFactory } from '@domain/company/application/factory/CompanyUseCaseFactory';
+import { CompanyController } from './controllers/company/company.controller';
+import { ProductController } from './controllers/product/product.controller';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [],
-  providers: [],
+  controllers: [CompanyController, ProductController],
+  providers: [CompanyUseCaseFactory],
 })
 export class HttpModule {}
